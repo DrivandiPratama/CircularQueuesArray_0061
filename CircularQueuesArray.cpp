30d993e2f4d719acc5c65ca2e05e7f128b782ef7
DrivandiPratama <dripandipratama@gmail.com>
2025-06-12 17:53:49 +0700

    commit 9

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -38,5 +38,15 @@ public:
             FRONT = 0; // 2.a
             REAR = 0; // 2.b
         }
- 
+        else
+        {
+            // jika rear berada did posisi terakhir array. kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else   
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
+    }
+
 };
