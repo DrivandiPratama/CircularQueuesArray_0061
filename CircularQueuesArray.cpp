cfbd7e463ea6d758740e76d719fa5d43c3ee6846
DrivandiPratama <dripandipratama@gmail.com>
2025-06-12 18:58:45 +0700

    commit 16

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -101,5 +101,24 @@ public:
         }
         else
         {
+            // jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir arrau
+            while (FRONT_position <= max - 1)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+
+            FRONT_position = 0;
+
+            // iterasi dari awal array hingga REAR
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
+    }
+};
 
 }
